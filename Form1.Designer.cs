449316a630b52883f49a3a68b14a8ac40c2fcebc
James Shoemaker <james.shoemaker99@gmail.com>
2020-10-27 13:59:38 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MoveEstimator
+{
+    partial class MoveEstimator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHrs = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMls = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblMove = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Hours Estimate:";
+            // 
+            // txtHrs
+            // 
+            this.txtHrs.Location = new System.Drawing.Point(391, 47);
+            this.txtHrs.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtHrs.Name = "txtHrs";
+            this.txtHrs.Size = new System.Drawing.Size(247, 22);
+            this.txtHrs.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(89, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Miles Estimate:";
+            // 
+            // txtMls
+            // 
+            this.txtMls.Location = new System.Drawing.Point(391, 110);
+            this.txtMls.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMls.Name = "txtMls";
+            this.txtMls.Size = new System.Drawing.Size(247, 22);
+            this.txtMls.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.Yellow;
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.ForeColor = System.Drawing.Color.Blue;
+            this.btnCalculate.Location = new System.Drawing.Point(95, 196);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(244, 38);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Estimate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lblMove
+            // 
+            this.lblMove.AutoSize = true;
+            this.lblMove.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMove.ForeColor = System.Drawing.Color.Blue;
+            this.lblMove.Location = new System.Drawing.Point(95, 276);
+            this.lblMove.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMove.Name = "lblMove";
+            this.lblMove.Size = new System.Drawing.Size(0, 25);
+            this.lblMove.TabIndex = 5;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.ForeColor = System.Drawing.Color.Red;
+            this.lblPrice.Location = new System.Drawing.Point(95, 417);
+            this.lblPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(0, 25);
+            this.lblPrice.TabIndex = 6;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(492, 320);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 17);
+            this.lblMessage.TabIndex = 7;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.ForeColor = System.Drawing.Color.Blue;
+            this.lblDisplay.Location = new System.Drawing.Point(477, 217);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(0, 17);
+            this.lblDisplay.TabIndex = 8;
+            // 
+            // MoveEstimator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblMove);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtMls);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHrs);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MoveEstimator";
+            this.Text = "Move Estimator GUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHrs;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMls;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblMove;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblDisplay;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Program Name: MoveEstimator
+//Author: James Shoemaker
+//Date written: April 23, 2019
+//Purpose: Week 3 Assignment
+
+
+namespace MoveEstimator
+{
+    public partial class MoveEstimator : Form
+    {
+        public MoveEstimator()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            double hours = double.Parse(txtHrs.Text);
+            double miles = double.Parse(txtMls.Text);
+
+            //Calculate total
+            double total = 200 + (150 * hours) + (2 * miles);
+
+            lblDisplay.Text = "For a move taking " +  hours  +  " hours and going "  +  miles  + " miles...";
+
+            lblMessage.Text = "The total is: " + total.ToString("C");
+
+
+        }
+    }
+}
